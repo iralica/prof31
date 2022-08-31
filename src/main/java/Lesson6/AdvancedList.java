3af14c8b3d7f8ebbcd597cea239a676aaa47ba75
comp1 <kiralica01112004@gmail.com>
2022-08-31 22:05:02 +0300

    8

--- src/main/java/Lesson6/AdvancedList.java
+++ src/main/java/Lesson6/AdvancedList.java
@@ -1,5 +1,20 @@
 package Lesson6;
 
 public interface AdvancedList {
+// Data structures definition
+    // Interfaces. Data structure 'List'. ArrayList implementation, beginning.
 
+    // List - упорядоченный контейнер элементов
+    //      ArrayList -
+    //      LinkedList -
+    //      Set - набор неповторяющихся элементов
+    //          HashSet
+    //          TreeSet - отсортированный порядок элементов
+
+    // Map key -> value
+    //          HashSet
+    //          TreeSet
+
+    // Queue
+    // Deque
 }
