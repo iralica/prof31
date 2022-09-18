d0359da3e1d12c69400a225e43ff8310045edab7
iralica <106344055+iralica@users.noreply.github.com>
2022-09-18 20:47:19 +0300

    Update SetComparator.java

--- src/main/java/Lesson12/homework/SetComparator.java
+++ src/main/java/Lesson12/homework/SetComparator.java
@@ -9,31 +9,25 @@ import java.util.Set;
 // использовать любые операции кроме Set.equals()
 public class SetComparator {
     public static void main(String[] args) {
-        Set<String> s1 = new HashSet<>(Arrays.asList("One", "Two", "Three", "Four", "Five"));
-        Set<String> s2 = new HashSet<>(Arrays.asList("One", "Two", "Three", "Four", "Five"));
-        long before = System.currentTimeMillis();
+        Set<String> s1 = new HashSet<>(Arrays.asList("Ann", "Tim", "Tom", "Sven", "Philip"));
+        Set<String> s2 = new HashSet<>(Arrays.asList("Ann", "Tim", "Tom", "Sven", "Philip"));
+        Set<String> s3 = new HashSet<>(Arrays.asList("Anet", "Tim", "Tom", "Sven", "Philip"));
+        System.out.println(checkSets(s1, s1));
         System.out.println(checkSets(s1, s2));
-        long after = System.currentTimeMillis();
-        System.out.println(after-before);
-
+        System.out.println(checkSets(s1, s3));
     }
    public static boolean checkSets(Set<String> s1, Set<String> s2){
-       // 1 указывают ли s1 и s2 на один и тот-же объект
-       if(s1 == s2)
+       if(s1 == s2) // не один ли объект
            return true;
-       // 2 не null оба
-       if(s1 == null || s2 == null)
+       if(s1 == null || s2 == null) // не равны ли нулю
            return false;
-       // 3 сеты разного размера не равны
-       if(s1.size() != s2.size())
+       if(s1.size() != s2.size()) // если разные размеры соответсвено не равны
            return false;
-       // 4 все элементы одного сета содержаться в другом сете
        for(String string:s1)
        {
            if(!s2.contains(string))
                return false;
        }
        return true;
-   }
 
 }
