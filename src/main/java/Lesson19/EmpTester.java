8c85c456824968d4b14663c0e847f0b21f6f1dc5
comp1 <kiralica01112004@gmail.com>
2022-10-10 22:05:04 +0300

    lesson 19 homework

--- src/main/java/Lesson19/EmpTester.java
+++ src/main/java/Lesson19/EmpTester.java
@@ -1,8 +1,12 @@
 package Lesson19;
 
+import com.sun.jdi.Value;
+
+import java.security.Key;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.OptionalDouble;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -11,7 +15,7 @@ public class EmpTester {
         List<Emp> employees = List.of(
                 new Emp("Max Petrov", 22, "programmer"),
                 new Emp("Ivan Shapovalov", 33, "analyst"),
-                new Emp("Semen Dezhnev", 55, "manger"),
+                new Emp("Semen Dezhnev", 55, "manager"),
                 new Emp("Oleg Petrov", 19, "intern"),
                 new Emp("Katerina Drogova", 31, "programmer"),
                 new Emp("Nikolas Spivakov", 23, "analyst"),
@@ -61,29 +65,48 @@ public class EmpTester {
         System.out.println(count);
 
         // посчитайте средний возраст
-        double averageAge = employees.stream()
-                    .mapToInt(emp-> (emp.getAge()/2)).sum();
-
+        OptionalDouble averageAge = employees.stream()
+                .mapToInt(emp-> emp.getAge())
+                .average();
         System.out.println(averageAge);
-
 
         // разделите на две группы - старше 40 лет и младше
         // найдите профессию самого "старого" из "молодых"
 
-       // Map<Boolean, List<Emp>> oldYoung = employees.stream()
-         //       .collect(Collectors.partitioningBy(emp -> emp.getAge() > 40));
+       Map<Boolean, List<Emp>> oldYoung = employees.stream()
+               .collect(Collectors.partitioningBy(emp -> emp.getAge() > 40));
 
+        System.out.println(oldYoung);
+
+        oldYoung.get(false)
+                .stream()
+                .max(Comparator.comparing(Emp::getAge))
+                .ifPresent(System.out::println);
 
         // сгруппируйте по профессии
-        // Collectors.groupingBy(new Function<Object, Object>() {})
-
+         Map<String, List<Emp>> pos = employees.stream()
+                 .collect(Collectors.groupingBy(emp -> emp.getPosition()));
+         System.out.println(pos);
         // распечатать профессии и количество работников в ней
+        Map<String, Long> countProfEmp =
+                employees.stream()
+                        .collect(Collectors.groupingBy(Emp::getPosition, Collectors.counting()));
+
+        System.out.println(countProfEmp);
 
         // вернуть средний возраст мужчин и женщин - у женщин фамилия оканчивается на "a"
+        employees.stream()
+                .collect(Collectors.groupingBy(e -> e.getName().charAt(e.getName().length() - 1) == 'a'))
+                    .forEach((key, value) ->
+                            System.out.println
+                                    (value.stream()
+                                            .mapToInt(Emp::getAge).average()));
 
         // распечатать работников с самым часто встречающимся возрастом
+        Map<Integer, Long> countOffenAge =
+                employees.stream().collect(Collectors.groupingBy(Emp::getAge, Collectors.counting()));
 
-
+        System.out.println(countOffenAge);
     }
 
 }
